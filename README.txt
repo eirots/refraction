ceabb7513515041d3eec5bb4e36e43b12e459c51
astorie <adstorie07@gmail.com>
2023-04-10 23:37:32 -0600

    added README.txt

---
+++ README.txt
@@ -0,0 +1,9 @@
+JavaFX JDK download: 
+	https://gluonhq.com/products/javafx/
+	be very careful about which download you grab; make sure both your operating system and architecture is correct, otherwise rendering won't work correctly. 
+	
+Instructions on how to add JavaFX to the buildpath: 
+	https://blog.floriancourgey.com/2020/06/javafx-installation-windows-macos
+
+Simple hello world to test your JavaFX installation. Alternatively, you can just run JFXHelloWorld.java in the testSandbox package. 
+	https://docs.oracle.com/javafx/2/get_started/hello_world.htm
---
+++ src/testSandbox/JFXHelloWorld.java
@@ -0,0 +1,36 @@
+package testSandbox;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class JFXHelloWorld extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Hello world");
+		Button btn = new Button();
+		btn.setText("Say 'Hello World'");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Hello world");
+			}
+		});
+
+		StackPane root = new StackPane();
+
+		root.getChildren().add(btn);
+		primaryStage.setScene(new Scene(root, 300, 250));
+		primaryStage.show();
+	}
+
+}
