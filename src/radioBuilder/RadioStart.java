210292ad72541e4bde5489b81639a86f5e6125a9
astorie <adstorie07@gmail.com>
2023-04-18 01:46:49 -0600

    midwork commit

--- src/radioBuilder/RadioStart.java
+++ src/radioBuilder/RadioStart.java
@@ -1,5 +1,40 @@
-package frontEnd;
+package radioBuilder;
 
-public class RadioSelection {
+import generalFrontEnd.ContinueButton;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+
+public class RadioStart {
+
+	private Scene scene;
+	private Pane pn;
+	private ContinueButton btn = new ContinueButton(); //TODO change to a larger 'continue' button class
+	private Text txt = new Text("How large would you like your glass to be?");
+	
+	public RadioStart() {
+		this.pn = new Pane();
+		setFormatting();
+		
+		pn.getChildren().addAll(txt, btn);
+		this.scene = new Scene(pn);
+
+		
+	}
+	
+	private void setFormatting() {
+		btn.setText("Test button");
+		btn.setLayoutX(200); btn.setLayoutY(400);
+		
+		txt.setLayoutX(200); txt.setLayoutY(300);
+		txt.setTextAlignment(TextAlignment.CENTER);
+	}
+
+	public Scene getScene() {
+		return scene;
+	}
 
 }
