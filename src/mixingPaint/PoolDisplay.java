36ea3f8b507ac515e2855da116e77dab03d21337
astorie <adstorie07@gmail.com>
2023-04-22 21:21:44 -0600

    updating

---
+++ src/mixingPaint/PoolDisplay.java
@@ -0,0 +1,14 @@
+package mixingPaint;
+
+import generalFrontEnd.RefractionScene;
+import javafx.scene.Scene;
+
+public class PoolDisplay implements RefractionScene {
+
+	@Override
+	public Scene getScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
