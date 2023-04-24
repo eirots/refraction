bce964c25c006d69aeb29ad5634c483cb02cc12e
astorie <adstorie07@gmail.com>
2023-04-23 21:23:53 -0600

    changing sizing

--- src/frontEnd/StartApplication.java
+++ src/frontEnd/StartApplication.java
@@ -4,8 +4,13 @@ import generalFrontEnd.Film;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Starts the application with mandatory arguments
+ * @author astorie
+ *
+ */
 public class StartApplication extends Application {
-	private double stageX = 500;private double stageY = 500;
+	private double stageX = 700;private double stageY = 700;
 	public static Stage window;
 	
 	public static void main(String[] args) {
